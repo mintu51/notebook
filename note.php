29155d52fdf14e23ef8da91f73298e805fea3398
Mintu <130635671+mintu51@users.noreply.github.com>
2023-04-19 08:00:07 +0530

    Update note.php

--- note.php
+++ note.php
@@ -1,1 +1,11 @@
-<h1>Hello</h1>
+*WordPress ShortCode*
+
+Normal Post
+<?php the_title();?>
+<?php the_content();?>	
+<?php echo get_the_post_thumbnail_url(); ?>
+
+Custome Post Post
+<?php the_field('welcome_title'); ?>
+<?php the_field('welcome_text'); ?>
+<?php the_field('welcome_img'); ?>
